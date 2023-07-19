0703b76610ff98b6991f350a801523eb046b4a06
Eric Long <i@hack3r.moe>
2023-07-20 01:12:39 +0800

    Organize scripts

--- prepare.sh
+++ prepare.sh
@@ -4,28 +4,22 @@ source metadata.sh
 
 _dirname=$_pkgname-$_electron_ver
 _dirpath=$(realpath $_dirname)
-cd $_dirname
+cd $_dirname/src
 
-# remove unused files (mainly to shrink size)
-readarray -t _files_excluded < ../files-excluded
-for _f in ${_files_excluded[@]}; do
-  rm -rfv src/$_f
-done
+# apply patches
+ln -s ../../patches patches
+quilt push -a
 
 # use system node w/out patching source; upstream hardcodes x64 in path
-mkdir -p src/third_party/node/linux/node-linux-x64/bin
-ln -sf /usr/bin/node src/third_party/node/linux/node-linux-x64/bin
+mkdir -p third_party/node/linux/node-linux-x64/bin
+ln -sf /usr/bin/node third_party/node/linux/node-linux-x64/bin
 
 # prefer unbundled (system) libraries
 # ./debian/scripts/unbundle
-pushd src
 ../../unbundle.py
-popd
 
-src/electron/script/apply_all_patches.py src/electron/patches/config.json
+( cd .. && \
+  src/electron/script/apply_all_patches.py src/electron/patches/config.json )
 
-# link patches folder
-# quilt push -a
-
-cd src/electron
+cd electron
 yarnpkg install --frozen-lockfile
--- source.sh
+++ source.sh
@@ -2,26 +2,24 @@ set -e
 shopt -s dotglob
 source metadata.sh
 
+_root=$PWD
 _dirname=$_pkgname-$_electron_ver
 rm -rf $_dirname
 mkdir $_dirname{,/src}
 _dirpath=$(realpath $_dirname)
 cd $_dirname
 
-pushd ../repos/chromium
-git checkout $_chromium_ver
-cp -r --reflink=auto * $_dirpath/src
-popd
+( cd ../repos/electron && git checkout v$_electron_ver )
 
-pushd ../repos/electron
-git checkout v$_electron_ver
-popd
+( cd ../repos/chromium && \
+  git checkout $_chromium_ver && \
+  cp -r --reflink=auto * $_dirpath/src )
 
 cat > .gclient <<EOF
 solutions = [
   {
     "name": "src/electron",
-    "url": "file://$(dirname $_dirpath)/repos/electron@v$_electron_ver",
+    "url": "file://$_root/repos/electron@v$_electron_ver",
     "deps_file": "DEPS",
     "managed": False,
     "custom_deps": {
@@ -33,27 +31,32 @@ solutions = [
 EOF
 
 _oldpath=$PATH
-export PATH+=":$(dirname $_dirpath)/repos/depot_tools"
+export PATH+=":$_root/repos/depot_tools"
 export DEPOT_TOOLS_UPDATE=0
 
-gclient sync -D \
-  --nohooks \
-  --with_branch_heads \
-  --with_tags
+gclient sync -D --nohooks --with_branch_heads --with_tags
+
+cd src
 
 echo "Running hooks..."
-python3 src/build/landmines.py
-python3 src/build/util/lastchange.py -o src/build/util/LASTCHANGE
-python3 src/build/util/lastchange.py -m GPU_LISTS_VERSION \
-  --revision-id-only --header src/gpu/config/gpu_lists_version.h
-python3 src/build/util/lastchange.py -m SKIA_COMMIT_HASH \
-  -s src/third_party/skia --header src/skia/ext/skia_commit_hash.h
-python3 src/build/util/lastchange.py \
-  -s src/third_party/dawn --revision src/gpu/webgpu/DAWN_VERSION
-python3 src/tools/update_pgo_profiles.py --target=linux update \
+python3 build/landmines.py
+python3 build/util/lastchange.py -o build/util/LASTCHANGE
+python3 build/util/lastchange.py -m GPU_LISTS_VERSION \
+  --revision-id-only --header gpu/config/gpu_lists_version.h
+python3 build/util/lastchange.py -m SKIA_COMMIT_HASH \
+  -s third_party/skia --header skia/ext/skia_commit_hash.h
+python3 build/util/lastchange.py \
+  -s third_party/dawn --revision gpu/webgpu/DAWN_VERSION
+python3 tools/update_pgo_profiles.py --target=linux update \
   --gs-url-base=chromium-optimization-profiles/pgo_profiles
 download_from_google_storage --no_resume --extract --no_auth \
-  --bucket chromium-nodejs -s src/third_party/node/node_modules.tar.gz.sha1
+  --bucket chromium-nodejs -s third_party/node/node_modules.tar.gz.sha1
 
 export PATH=$_oldpath
 unset DEPOT_TOOLS_UPDATE
+
+# remove unused files (mainly to shrink size)
+readarray -t _files_excluded < ../../files-excluded.txt
+for _f in ${_files_excluded[@]}; do
+  rm -rfv $_f
+done
