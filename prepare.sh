cc71bc0b8dde3172bca2b069e721fb3212721cec
Eric Long <i@hack3r.moe>
2023-07-20 18:09:00 +0800

    Replace bundled OpenJDK with system one

--- prepare.sh
+++ prepare.sh
@@ -14,6 +14,9 @@ quilt push -a
 mkdir -p third_party/node/linux/node-linux-x64/bin
 ln -sf /usr/bin/node third_party/node/linux/node-linux-x64/bin
 
+# same for openjdk
+ln -s /usr/lib/jvm/java-11-openjdk-$(dpkg-architecture -q DEB_HOST_ARCH) \
+  third_party/jdk/current
+
 # prefer unbundled (system) libraries
-# ./debian/scripts/unbundle
 ../../unbundle.py
