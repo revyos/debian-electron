9c121f61d1ffffa88882ea19a8ce843a8df7816a
Eric Long <i@hack3r.moe>
2023-07-20 20:55:31 +0800

    prepare: Create JDK directory

--- prepare.sh
+++ prepare.sh
@@ -15,6 +15,7 @@ mkdir -p third_party/node/linux/node-linux-x64/bin
 ln -sf /usr/bin/node third_party/node/linux/node-linux-x64/bin
 
 # same for openjdk
+mkdir third_party/jdk
 ln -s /usr/lib/jvm/java-11-openjdk-$(dpkg-architecture -q DEB_HOST_ARCH) \
   third_party/jdk/current
 
